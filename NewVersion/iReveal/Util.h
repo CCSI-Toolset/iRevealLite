7a6bb4b9c04fe5c1ac20a696ee51e6574089ad6b
jinliangma <jinliang.ma@netl.doe.gov>
2018-02-16 16:48:40 -0600

    Revised Util.h and Util.cpp along with docs

--- NewVersion/iReveal/Util.h
+++ NewVersion/iReveal/Util.h
@@ -1,8 +1,10 @@
 //Util.h
+#include "CCSI.h"
 
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
+string GetExecutableDirectory();
 void PrintCommandUsage();
 int ProcessJsonAndSampleInputSpace(char* filename);
 int BuildKrigingRom();
